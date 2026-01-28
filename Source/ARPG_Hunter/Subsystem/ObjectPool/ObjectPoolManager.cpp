94f5a17b77d641acf73df22e1d98a13379cb2198
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-01-29 01:35:44 +0900

    Feat: 액터용 오브젝트 풀 추가

--- Source/ARPG_Hunter/Subsystem/ObjectPool/ObjectPoolManager.cpp
+++ Source/ARPG_Hunter/Subsystem/ObjectPool/ObjectPoolManager.cpp
@@ -1,8 +1,92 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Subsystem/ObjectPool/ObjectPoolManager.h"
 
+UActorObjectPool::UActorObjectPool()
+{
+}
+
+void UActorObjectPool::Init(TSubclassOf<AActor> _actorClass, int _initialSize)
+{
+	ActorClass = _actorClass;
+	Container.Reserve(_initialSize);
+
+	for (int i = 0; i < _initialSize; ++i)
+	{
+		TObjectPtr<AActor> inst = Create();
+		DeactivateActor(inst);
+	}
+}
+
+TObjectPtr<AActor> UActorObjectPool::Create()
+{
+	TObjectPtr<AActor> inst = GetWorld()->SpawnActor(ActorClass);
+
+	Container.Add(inst);
+	Pool.Enqueue(inst);
+
+	return inst;
+}
+
+void UActorObjectPool::ActivateActor(TObjectPtr<AActor> _actor)
+{
+	_actor->SetActorHiddenInGame(false);
+	_actor->SetActorEnableCollision(true);
+}
+
+void UActorObjectPool::DeactivateActor(TObjectPtr<AActor> _actor)
+{
+	_actor->SetActorHiddenInGame(true);
+	_actor->SetActorEnableCollision(false);
+}
+
+TObjectPtr<AActor> UActorObjectPool::Get()
+{
+	TObjectPtr<AActor> inst;
+
+	if (Pool.IsEmpty())
+		inst = Create();
+	else
+		Pool.Dequeue(inst);
+
+	ActivateActor(inst);
+
+	return inst;
+}
+
+void UActorObjectPool::Release(TObjectPtr<AActor> _instance)
+{
+	DeactivateActor(_instance);
+	Pool.Enqueue(_instance);
+}
+
+
 UObjectPoolManager::UObjectPoolManager()
 {
+}
+
+void UObjectPoolManager::RegisterObject(TSubclassOf<AActor> _class, int _initialSize)
+{
+	if (MapObjectPool.Find(_class) != nullptr)
+		return;
+
+	TObjectPtr<UActorObjectPool> Pool = MapObjectPool.Add(_class.Get());
+	Pool->Init(_class, _initialSize);
+}
+
+TObjectPtr<AActor> UObjectPoolManager::Get(TSubclassOf<AActor> _class)
+{
+	if (MapObjectPool.Find(_class.Get()) == nullptr)
+		return nullptr;
+
+	return MapObjectPool[_class.Get()]->Get();
+}
+
+void UObjectPoolManager::Release(TObjectPtr<AActor> _inst)
+{
+	if (MapObjectPool.Find(_inst.GetClass()) == nullptr)
+		return;
+
+	MapObjectPool[_inst.GetClass()]->Release(_inst);
 }
--- Source/ARPG_Hunter/Subsystem/ObjectPool/ObjectPoolManager.h
+++ Source/ARPG_Hunter/Subsystem/ObjectPool/ObjectPoolManager.h
@@ -1,11 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Subsystems/WorldSubsystem.h"
-#include "Containers/Queue.h"
 #include "ObjectPoolManager.generated.h"
+
+
+
+UCLASS()
+class ARPG_HUNTER_API UActorObjectPool : public UObject
+{
+	GENERATED_BODY()
+public:
+	UActorObjectPool();
+
+private:
+	TSubclassOf<AActor> ActorClass;
+
+	UPROPERTY()
+	TArray<TObjectPtr<AActor>> Container;
+	TQueue<TObjectPtr<AActor>> Pool;
+
+	TObjectPtr<AActor> Create();
+	void ActivateActor(TObjectPtr<AActor> _actor);
+	void DeactivateActor(TObjectPtr<AActor> _actor);
+
+public:
+	void Init(TSubclassOf<AActor> _actorClass, int _initialSize);
+	TObjectPtr<AActor> Get();
+	void Release(TObjectPtr<AActor> _instance);
+};
 
 /**
  * 
@@ -14,13 +39,14 @@ UCLASS()
 class ARPG_HUNTER_API UObjectPoolManager : public UWorldSubsystem
 {
 	GENERATED_BODY()
-
 public:
 	UObjectPoolManager();
-
 private:
 	UPROPERTY()
-	TQueue<TObjectPtr<AActor>> Pool;
-	
+	TMap<UClass*, TObjectPtr<UActorObjectPool>> MapObjectPool;
 
+public:
+	void RegisterObject(TSubclassOf<AActor> _class, int _initialSize = 30);
+	TObjectPtr<AActor> Get(TSubclassOf<AActor> _class);
+	void Release(TObjectPtr<AActor> _inst);
 };
