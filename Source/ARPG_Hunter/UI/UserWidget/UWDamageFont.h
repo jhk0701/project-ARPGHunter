586e1b8d6ec7db826856434ffbc173a532e7bf97
sdasdasd <kjhwan0814@naver.com>
2026-01-28 18:11:41 +0900

    Chore: UI용 폰트 추가

---
+++ Source/ARPG_Hunter/UI/UserWidget/UWDamageFont.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UWDamageFont.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_HUNTER_API UUWDamageFont : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
