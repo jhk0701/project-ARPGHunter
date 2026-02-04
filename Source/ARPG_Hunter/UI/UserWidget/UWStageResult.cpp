a231735b36df3453d4f79cc20c41635bb19a7cce
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-02-05 00:57:06 +0900

    Feat: 스테이지 종료 UI 추가

---
+++ Source/ARPG_Hunter/UI/UserWidget/UWStageResult.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/UserWidget/UWStageResult.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+
+
+void UUWStageResult::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	ReturnButton->OnClicked.AddDynamic(this, &UUWStageResult::ClickReturnButton);
+}
+
+
+void UUWStageResult::ShowUI()
+{
+	SetVisibility(ESlateVisibility::Visible);
+
+	if (OpenAnim)
+		PlayAnimation(OpenAnim);
+}
+
+void UUWStageResult::HideUI()
+{
+	SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UUWStageResult::Update(bool _bIsClear)
+{
+	ResultLabel->SetText(_bIsClear ? FText::FromString(TEXT("Stage Clear")) : FText::FromString(TEXT("Stage Fail")));
+}
+
+void UUWStageResult::ClickReturnButton()
+{
+	OnClickReturnButton.ExecuteIfBound();
+}
---
+++ Source/ARPG_Hunter/UI/UserWidget/UWStageResult.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UWStageResult.generated.h"
+
+class UTextBlock;
+class UButton;
+
+DECLARE_DELEGATE(FOnClickReturnButton);
+
+/**
+ * 
+ */
+UCLASS()
+class ARPG_HUNTER_API UUWStageResult : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> ResultLabel;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> RewardGoldLabel;
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> ReturnButton;
+
+	UPROPERTY(Transient, meta = (BindWidgetAnim))
+	TObjectPtr<UWidgetAnimation> OpenAnim;
+
+public:
+	FOnClickReturnButton OnClickReturnButton;
+
+	void ShowUI();
+	void HideUI();
+
+	void Update(bool _bIsClear);
+
+	UFUNCTION()
+	void ClickReturnButton();
+
+protected:
+	void NativeOnInitialized() override;
+};
