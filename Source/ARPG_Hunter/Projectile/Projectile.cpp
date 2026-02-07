c8ad177bdfd30493dc3e773d270ed28ad81c4b52
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-02-07 16:03:47 +0900

    Chore: 원거리형 캐릭터 추가

---
+++ Source/ARPG_Hunter/Projectile/Projectile.cpp
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile/Projectile.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	SetRootComponent(BoxComp);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(BoxComp);
+
+	BoxComp->SetCollisionProfileName(FName(TEXT("OverlapPlayer")));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::OnBeginOverlap);
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AProjectile::Init(TObjectPtr<class UStaticMesh> _mesh)
+{
+	MeshComp->SetStaticMesh(_mesh);
+}
+
+void AProjectile::OnBeginOverlap(
+	UPrimitiveComponent* OverlappedComponent, 
+	AActor* OtherActor, 
+	UPrimitiveComponent* OtherComp, 
+	int32 OtherBodyIndex, 
+	bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
---
+++ Source/ARPG_Hunter/Projectile/Projectile.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class ARPG_HUNTER_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditDefaultsOnly);
+	TObjectPtr<class UBoxComponent> BoxComp;
+	UPROPERTY(EditDefaultsOnly);
+	TObjectPtr<class UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	float Speed{100.0f};
+	UPROPERTY(EditDefaultsOnly)
+	FVector TargetLocation;
+
+
+public:	
+	AProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+	
+	void Init(TObjectPtr<class UStaticMesh> _mesh);
+
+	UFUNCTION()
+	void OnBeginOverlap(
+		UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp, 
+		int32 OtherBodyIndex, bool bFromSweep, 
+		const FHitResult& SweepResult);
+};
