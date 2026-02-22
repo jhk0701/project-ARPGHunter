8a43605867bb9c73c9c7d5c16a8d6ec249b23774
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-02-22 20:39:58 +0900

    Update: Stage2 임시 배치

---
+++ Source/ARPG_Hunter/Misc/RandomMesh.cpp
@@ -0,0 +1,67 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Misc/RandomMesh.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+ARandomMesh::ARandomMesh()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	SetRootComponent(BoxComp);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	MeshComp->SetupAttachment(BoxComp);
+}
+
+void ARandomMesh::SetRandomMesh()
+{
+	if (MeshList.Num() == 0)
+		return;
+
+	int32 r = FMath::RandRange(0, MeshList.Num() - 1);
+	MeshComp->SetStaticMesh(MeshList[r]);
+
+	FVector Extent = MeshList[r]->GetBounds().BoxExtent;
+	BoxComp->SetBoxExtent(Extent);
+	MeshComp->SetRelativeLocation(FVector(0, 0, -Extent.Z));
+
+	if (bUpdateLocation)
+	{
+		FVector vecBoxSize = BoxComp->GetScaledBoxExtent();
+		FVector Loc = GetActorLocation();
+		Loc.Z -= vecBoxSize.Z;
+
+		SetActorLocation(FVector(Loc.X, Loc.Y, Loc.Z + vecBoxSize.Z));
+	}
+}
+
+void ARandomMesh::SetRandomTransform()
+{
+	FVector Loc = GetActorLocation() + FVector(
+		FMath::FRandRange(MinLocationOffset.X, MaxLocationOffset.X),
+		FMath::FRandRange(MinLocationOffset.Y, MaxLocationOffset.Y),
+		FMath::FRandRange(MinLocationOffset.Z, MaxLocationOffset.Z)
+	);
+
+	FRotator Rot = FRotator(
+		FMath::FRandRange(MinRotation.Pitch, MaxRotation.Pitch),
+		FMath::FRandRange(MinRotation.Yaw, MaxRotation.Yaw),
+		FMath::FRandRange(MinRotation.Roll, MaxRotation.Roll)
+	);
+
+	FVector Scale = FVector(
+		FMath::FRandRange(MinScale.X, MaxScale.X),
+		FMath::FRandRange(MinScale.Y, MaxScale.Y),
+		FMath::FRandRange(MinScale.Z, MaxScale.Z)
+	);
+
+	FTransform Transform;
+	Transform.SetLocation(Loc);
+	Transform.SetRotation(Rot.Quaternion());
+	Transform.SetScale3D(Scale);
+	SetActorTransform(Transform);
+}
+
---
+++ Source/ARPG_Hunter/Misc/RandomMesh.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RandomMesh.generated.h"
+
+UCLASS()
+class ARPG_HUNTER_API ARandomMesh : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UBoxComponent> BoxComp;
+
+	UPROPERTY(EditAnywhere, Category = "Utility")
+	TArray<TObjectPtr<UStaticMesh>> MeshList;
+	UPROPERTY(EditAnywhere, Category = "Utility")
+	bool bUpdateLocation{ false };
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FVector MinLocationOffset;
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FVector MaxLocationOffset;
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FRotator MinRotation;
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FRotator MaxRotation;
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FVector MinScale{ 1.0f,1.0f,1.0f };
+	UPROPERTY(EditAnywhere, Category = "Utility|RandomTransform")
+	FVector MaxScale{ 1.0f,1.0f,1.0f };
+
+public:	
+	ARandomMesh();
+
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Utility")
+	void SetRandomMesh();
+	UFUNCTION(BlueprintCallable, CallInEditor, Category = "Utility")
+	void SetRandomTransform();
+};
