3396efd24683cbb165cfb82644c807a9589d324e
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-02-25 14:18:11 +0900

    Feat: NPC 스크립트 추가

---
+++ Source/ARPG_Hunter/NPC/NPC.cpp
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NPC/NPC.h"
+
+ANPC::ANPC()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void ANPC::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/ARPG_Hunter/NPC/NPC.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class ARPG_HUNTER_API ANPC : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ANPC();
+
+private:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UNPCConfig> Config;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+};
