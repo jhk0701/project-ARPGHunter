73f9f6b689e39f1b0a971436aad927fa9daa4d44
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2026-03-26 18:23:26 +0900

    Update: 스태미너 수치 조절

--- Source/ARPG_Hunter/Stage/StageKillZone.cpp
+++ Source/ARPG_Hunter/Stage/StageKillZone.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "Stage/StageKillZone.h"
@@ -7,8 +7,7 @@
 AStageKillZone::AStageKillZone()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
